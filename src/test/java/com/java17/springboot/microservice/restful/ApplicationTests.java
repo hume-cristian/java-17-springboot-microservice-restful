46660380628fa261a5fd11ce7af785f112928ac9
hume-cristian <hume-cristian@outlook.com>
2024-11-09 22:15:14 -0300

    feat: Create project for Java 17 and Spring Boot 3.3.5

---
+++ src/test/java/com/java17/springboot/microservice/restful/ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.java17.springboot.microservice.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Cristian Hume Henriquez
+ * @version 1.0.0
+ * @since 2024-11-09
+ */
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
